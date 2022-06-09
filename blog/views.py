d71f05370cf72510a7dd34731981f2b3d0597942
Domnann <domnangambo@gmail.com>
2022-06-09 23:16:59 +0100

    modifed a template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+    {
+        'authour': 'DomnanG',
+        'title': 'Blog Post',
+        'content' : 'my first post',
+        'date_posted' : 'May 7, 2022'
+    },
+    {
+        'authour': 'Pinkfinger',
+        'title': 'Blog Post 2',
+        'content' : 'my second post',
+        'date_posted' : 'May 7, 2022'  
+    }
+]
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
+
+def about (request):
+    return render(request, 'blog/about.html')
 
