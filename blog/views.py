a3e0db7ef1259b5d33e0e00d3636b34bf84d9236
Domnann <domnangambo@gmail.com>
2022-06-10 07:56:42 +0100

    modified template, added some html, css and bootstrap, b;og now has a navigation bar and a sidebar.

--- blog/views.py
+++ blog/views.py
@@ -21,5 +21,5 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def about (request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html',{'title': 'About'})
 
