1b50929b08cb369329daca3ec421fafd08b932e8
Domnann <domnangambo@gmail.com>
2022-06-12 00:41:13 +0100

    worked on the user registeration page, styling with bootstrap and crispy, also linked it to django admin

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from users.views import register
 from . import views
 
 urlpatterns = [
     path('', views.home, name= 'blog-home'),
     path('about/', views.about, name='blog-about'),
+    
 ]
 
