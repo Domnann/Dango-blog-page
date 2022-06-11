7fb920d983874f0361132d0ef3bc3229c92fcae9
Domnann <domnangambo@gmail.com>
2022-06-11 16:22:06 +0100

    worked on django blog post, user post, django admin

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts=[
-    {
-        'authour': 'DomnanG',
-        'title': 'Blog Post',
-        'content' : 'my first post',
-        'date_posted' : 'May 7, 2022'
-    },
-    {
-        'authour': 'Pinkfinger',
-        'title': 'Blog Post 2',
-        'content' : 'my second post',
-        'date_posted' : 'May 7, 2022'  
-    }
-]
 
 def home(request):
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 def about (request):
