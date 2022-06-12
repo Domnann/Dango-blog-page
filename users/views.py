cd77c5d55b7f7f0a4b78aab5a5a15a80f12b36a3
Domnann <domnangambo@gmail.com>
2022-06-12 02:11:41 +0100

    worked on user  login page

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from email import message
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .form import UserRegisterForm
 
 def register(request):
@@ -10,10 +11,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username= form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}.')
-            return redirect ('blog-home')
+            messages.success(request, f'Hey, {username} your account has been created, you can now log in.')
+            return redirect ('login')
     else:
         form= UserRegisterForm()
     return render (request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
+
 
 
