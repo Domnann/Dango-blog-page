9f0bda77cea713db33298e7ca7944b5f74976bb6
Domnann <domnangambo@gmail.com>
2022-06-13 10:39:07 +0100

    worked on the profile and login and sign up pages, as well as using signal.py to make profile saved when the user logs in

--- pink_django/settings.py
+++ pink_django/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for pink_django project.
 
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pink_django/urls.py
+++ pink_django/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
