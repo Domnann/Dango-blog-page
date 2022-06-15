6b6c3bc3fabac269927bbf057f9f0e72c56a1104
Domnann <domnangambo@gmail.com>
2022-06-15 23:28:36 +0100

    add a root link redirecting to detailed listed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from users.views import register
+from .views import PostDetailView, PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name= 'blog-home'),
+    path('', PostListView.as_view(), name= 'blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name= 'post-detail'),
     path('about/', views.about, name='blog-about'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,6 +8,17 @@ def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name= 'blog/home.html'         #<app>/mdodel>_<viewtype>.html ||List view vs CLass view
+    context_object_name= "posts"
+    ordering= ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
 def about (request):
     return render(request, 'blog/about.html',{'title': 'About'})
+
+
 
