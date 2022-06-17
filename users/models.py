1790c106e61304346f44e16d8b55d5c8d5184375
Domnann <domnangambo@gmail.com>
2022-06-17 07:39:29 +0100

    worked on the pagination of the blog post, also post can now be filtered by user

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
+    def save(self, *args, **kwargs):
         super().save()
 
         img = Image.open(self.image.path)
@@ -19,3 +19,6 @@ class Profile(models.Model):
             output_size= (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+
